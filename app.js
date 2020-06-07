70c426fc995de234d318b1c3fe910fa706be34d2
Alexander Profeit <alex@profeit.com>
2020-06-06 23:50:27 -0400

    make public

---
+++ app.js
@@ -0,0 +1,42 @@
+var d3 = require("d3");
+var cloud = require("d3-cloud");
+var fs = require("fs");
+
+var file = fs.readFileSync(__dirname + '/data/top_words.json', 'utf8');
+
+var words = JSON.parse(file).map(function(word) {
+  return { text: word[0], size: word[1] }
+});
+
+var max = d3.max(words.map(function(word) {
+  return word.size
+}));
+
+var layout = cloud()
+  .size([1400, 800])
+  .words(words)
+  .padding(5)
+  .font("Impact")
+  .fontSize(function(d) { return (d.size / 1.5); })
+  .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+  d3.select("body").append("svg")
+    .attr("width", layout.size()[0])
+    .attr("height", layout.size()[1])
+    .append("g")
+    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+    .data(words)
+    .enter().append("text")
+    .style("font-size", function(d) { return d.size + "px"; })
+    .style("font-family", "Impact")
+    .style("fill", function(d) { return d3.interpolateOrRd(d.size / max)})
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    })
+    .text(function(d) { return d.text; });
+}
---
+++ transform.rb
@@ -0,0 +1,45 @@
+require 'json'
+
+NUMBER_OF_MESSAGE_FILES = 2
+WORD_LIMIT = 100
+
+def read_json(filename)
+  JSON.parse(File.read(filename))
+end
+
+def reencode(string)
+  string.encode('iso-8859-1').force_encoding('utf-8')
+end
+
+stop_words = read_json('data/stopwords.json')['words'].map do |word|
+  reencode(word)
+end
+
+words = (1..NUMBER_OF_MESSAGE_FILES).map do |number|
+  JSON.parse(File.read("data/message_#{number}.json"))['messages'].reject do |message|
+    message['content'].nil?
+  end.sort_by do |message|
+    message['timestamp_ms']
+  end.map do |message|
+    message['content'].encode('iso-8859-1').force_encoding('utf-8')
+  end
+end.flatten.map do |message|
+  message.split.map do |word|
+    word.downcase.tr('.', '').tr('?', '').tr('"', '').sub('’', "'").tr('(', '').tr(')', '').tr('!', '').tr('@', '').tr(',', '')
+  end.flatten
+end.flatten.reject do |word|
+  stop_words.include?(word) || word.include?('http') || word.empty? || word.include?('🍑')
+end.reject do |word|
+  %w{1 2 3 4 5 6 7 8 9 10}.include?(word)
+end
+
+top_words = Hash.new(0)
+words.each do |word|
+  top_words[word] += 1
+end
+
+top_words = top_words.sort_by do |(word, count)|
+  count
+end.reverse.first(WORD_LIMIT)
+
+File.write('data/top_words.json', top_words.to_json)
